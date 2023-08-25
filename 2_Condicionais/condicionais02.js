de74f09fbd8a881048711ec54be503a08ec00fc6
Pedro Paulo <silva.pedro1155@gmail.com>
2023-08-25 12:27:28 -0300

    Aula de if else

---
+++ 2_Condicionais/condicionais02.js
@@ -0,0 +1,8 @@
+var selecao = 'Chile'
+
+if (selecao == 'Brasil' || selecao == 'Argentina' || selecao == 'Uruguai'){
+    console.log(`${selecao} já ganhou a copa do mundo!`)
+}
+    else{
+        console.log(`${selecao} nunca ganhou uma copa do mundo`)
+    }
---
+++ 2_Condicionais/condicionais03.js
@@ -0,0 +1,11 @@
+var selecao = 'Argentina'
+
+if (selecao == 'Brasil'){
+    console.log(`${selecao} já ganhou a copa do mundo cinco vezes!`)
+}
+    else if(selecao == 'Argentina' || selecao == 'Uruguai'){
+        console.log(`${selecao} já ganhou a copa do mundo duas vezes.`)
+    }
+    else{
+        console.log(`${selecao} nunca ganhou uma copa do mundo`)
+    }
